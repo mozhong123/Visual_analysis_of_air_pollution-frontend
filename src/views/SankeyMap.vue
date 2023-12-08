526ff5ddea41edf7dad1f10670fc1a7dea865f46
C_LLI <1678037507@qq.com>
2023-12-08 18:51:17 +0800

    'graph_position'

--- src/views/SankeyMap.vue
+++ src/views/SankeyMap.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>SankeyMap</h1>
   <div class="container-fluid" :style="containerStyle">
     <div class="row" :style="rowStyle">
       <div id="pcpaddr" class="col-4">
@@ -30,7 +31,7 @@ export default {
     return {
       containerStyle: {
         backgroundColor: "rgba(0,0,0,0)",
-        height: "calc(78vh)", // 根据需要调整额外的边距
+        height: "calc(50vh)", // 根据需要调整额外的边距
         width: "800px",
       },
       rowStyle: {
